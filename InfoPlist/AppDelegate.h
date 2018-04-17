6c1010836b0fe5aed680047f98d56e2a4c1f84f9
李敏 <1512510162@qq.com>
2018-04-17 13:15:25 +0800

    Initial Commit

---
+++ InfoPlist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InfoPlist
+//
+//  Created by 李敏 on 2018/4/17.
+//  Copyright © 2018年 Tongle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InfoPlist/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InfoPlist
+//
+//  Created by 李敏 on 2018/4/17.
+//  Copyright © 2018年 Tongle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
